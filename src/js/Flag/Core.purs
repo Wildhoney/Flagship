7269d8d289a1a9c8d5ad781c9373a2d1d0cf1b6d
Wildhoney <adam.timberlake@gmail.com>
2017-05-23 20:30:10 +0100

    Added check for equality

--- src/js/Flag/Core.purs
+++ src/js/Flag/Core.purs
@@ -19,18 +19,22 @@ import Text.Smolder.HTML.Attributes (src, alt, className, disabled)
 
 type Name = String
 type Countries = Array Name
+type CorrectAnswer = Name
 
 type State = { countries ∷ Countries, correct ∷ Int, incorrect ∷ Int }
-data Event = Answer Name Name | RequestCountries | ReceiveCountries (Either Error Countries)
+data Event = Answer Name | RequestCountries | ReceiveCountries (Either Error Countries)
 
 init ∷ State
 init = { countries: [], correct: 0, incorrect: 0 }
 
 foldp ∷ Event → State → EffModel State Event (random ∷ RANDOM)
-foldp (Answer a b) st
-  | a == b    = noEffects $ st { countries = drop 1 st.countries, correct = st.correct + 1 }
-  | a /= b    = noEffects $ st { countries = drop 1 st.countries, incorrect = st.incorrect + 1 }
-  | otherwise = noEffects $ st { countries = drop 1 st.countries }
+foldp (Answer name) st = noEffects $ case head st.countries of
+  Nothing      -> st { countries = [], correct = 0, incorrect = 0 }
+  Just correct -> if name == correct then isCorrect else isIncorrect
+    where
+      countries   = drop 1 st.countries
+      isCorrect   = st { countries = countries, correct = st.correct + 1 }
+      isIncorrect = st { countries = countries, incorrect = st.incorrect + 1 }
 foldp (ReceiveCountries (Left err)) st = noEffects $ st { countries = [] }
 foldp (ReceiveCountries (Right xs)) st = noEffects $ st { countries = xs }
 foldp (RequestCountries)            st = { state: st, effects: [do
@@ -62,8 +66,8 @@ flag state = div ! className "prompt" $ do
   case head state.countries of
     Just name -> do
       img ! src ("images/flags/" <> name <> ".svg") ! alt "Flag"
-      ul ! className "countries" $ for_ state.countries $ choice name
+      ul ! className "countries" $ for_ state.countries $ choice
     Nothing   -> div $ text "All done!"
 
-choice ∷ Name → Name → HTML Event
-choice answer name = li $ button #! onClick (const $ Answer answer name) $ text name
+choice ∷ Name → HTML Event
+choice name = li $ button #! onClick (const $ Answer name) $ text name
