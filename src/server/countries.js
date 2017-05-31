d6e5859cc02fdad7722b096292402023ac6feef6
Wildhoney <adam.timberlake@gmail.com>
2017-05-31 23:27:21 +0100

    Filtering by svg

--- src/server/countries.js
+++ src/server/countries.js
@@ -2,17 +2,33 @@ import { readdirSync } from 'fs';
 import { parse } from 'path';
 import { titleCase } from 'change-case';
 
-const map = [
+/**
+ * @constant transformations
+ * @type {Array}
+ */
+const transformations = [
     { from: 'Guinea Bissau', to: 'Guinea-Bissau' }
 ];
 
+/**
+ * @method exceptions
+ * @param {String} name 
+ * @return {String}
+ */
 const exceptions = name => {
-    const model = map.find(item => item.from === name);
+    const model = transformations.find(item => item.from === name);
     return model ? model.to : name;
 };
 
+/**
+ * @method isImage
+ * @param {String} filename 
+ * @return {Boolean}
+ */
+const isImage = filename => /svg$/i.test(filename);
+
 export default () => {
-    return readdirSync(`${__dirname}/../images/flags`).map(filename => {
+    return readdirSync(`${__dirname}/../images/flags`).filter(isImage).map(filename => {
         return { name: exceptions(titleCase(parse(filename).name)), flag: filename };
     });
 }
