122a86b737ac4020b4580963881a5bc32d090e49
Wildhoney <adam.timberlake@gmail.com>
2017-06-10 20:54:25 +0100

    Added randomising of countries

--- src/js/Flag/Core.purs
+++ src/js/Flag/Core.purs
@@ -1,62 +1,74 @@
-module Flag.Core (view, foldp, init) where
+module Flag.Core (fetch, shuffle, view, foldp, init) where
 
-import Prelude hiding (div)
-import Flag.Types (Countries, Country(..), State, Event(..))
-import Control.Monad.Aff (attempt)
-import Control.Monad.Eff.Random (RANDOM)
-import Data.Argonaut (decodeJson)
-import Data.Array (uncons, length, drop, head, take)
+import Control.Monad.Aff (Aff, attempt)
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Random (RANDOM, random)
+import Data.Argonaut (decodeJson, Json)
+import Data.Array (length, replicate, sortBy, zip)
 import Data.Either (Either(..), either)
-import Data.Foldable (for_)
 import Data.Maybe (Maybe(..))
+import Data.Traversable (sequence)
+import Data.Tuple (Tuple(..), snd)
+import Flag.Types (Countries, Country(..), State, Event(..))
 import Network.HTTP.Affjax (AJAX, get)
 import Pux (EffModel, noEffects)
 import Pux.DOM.Events (onClick)
 import Pux.DOM.HTML (HTML)
-import Text.Smolder.HTML (h1, img, button, ul, li, div)
-import Text.Smolder.HTML.Attributes (src, alt, className, disabled)
+import Text.Smolder.HTML (h1, button, ul, li, div)
+import Text.Smolder.HTML.Attributes (className)
 import Text.Smolder.Markup (text, (!), (#!))
+import Prelude hiding (div)
 
 init ∷ State
-init = { countries: [], correct: 0, incorrect: 0 }
+init = { countries: Nothing, correct: 0, incorrect: 0 }
+
+shuffle ∷ ∀ e a. Array a → Eff (random :: RANDOM | e) (Array a)
+shuffle xs = do
+  randoms <- sequence $ replicate (length xs) random
+  pure $ map snd $ sortBy compareFst $ zip randoms xs
+  where compareFst (Tuple a _) (Tuple b _) = compare a b
+
+decode ∷ ∀ r. { response ∷ Json | r } → Either String (Array Country)
+decode request = decodeJson request.response :: Either String Countries
+
+fetch ∷ ∀ e. Aff (ajax ∷ AJAX | e) (Maybe Event)
+fetch = do
+  response <- attempt $ get "/countries.json"
+  let countries = either (Left <<< show) decode response
+  pure $ Just $ Receive $ case shuffle <$> countries of
+    Left _   → Nothing
+    Right xs → Just xs
 
 foldp ∷ Event → State → EffModel State Event (random ∷ RANDOM, ajax ∷ AJAX)
-foldp (Respond name)       state = noEffects $ case ((_ == name) <<< \(Country model) -> model.name) <$> head state.countries of
-  (Just true)  -> state { countries = drop 1 state.countries, correct = state.correct + 1 }
-  (Just false) -> state { countries = drop 1 state.countries, incorrect = state.incorrect + 1 }
-  Nothing      -> state { countries = [], correct = 0, incorrect = 0 }
-foldp (Receive (Left err)) state = noEffects $ state { countries = [] }
-foldp (Receive (Right xs)) state = noEffects $ state { countries = xs }
-foldp (Request)            state = { state: state, effects: [fetchCountries]}
-  where
-    decode transfer = decodeJson transfer.response :: Either String Countries
-    fetchCountries  = do
-      response <- attempt $ get "/countries.json"
-      let countries = either (Left <<< show) decode response
-      pure $ Just $ Receive countries
+foldp (Respond name)      state = noEffects $ state { countries = Nothing, correct = state.correct + 1 }
+foldp (Receive Nothing)   state = noEffects $ state { countries = Nothing }
+foldp (Receive (Just xs)) state = noEffects $ state { countries = Just xs }
+foldp (Request)           state = { state, effects: [fetch] }
 
 view ∷ State → HTML Event
-view state = div ! className "flagship" $ case uncons state.countries of
-  Nothing -> toolbar state
-  Just _  -> toolbar state <> country state
+view state = div ! className "flagship" $ case state.countries of
+  Nothing → toolbar state
+  Just _  → toolbar state <> country state
 
 toolbar ∷ State → HTML Event
 toolbar state = ul ! className "header" $ do
   li $ h1 $ text "Flagship"
-  li $ button ! disabled isDisabled #! onClick (const Request) $ text "Start"
+  li $ button #! onClick (const Request) $ text "Start"
   li ! className "score correct" $ text $ show state.correct
   li ! className "score incorrect" $ text $ show state.incorrect
   li ! className "score" $ text $ show $ state.correct + state.incorrect
-    where
-      isDisabled = if (length state.countries == 0) then "" else "disabled"
+    -- where isDisabled = state.countries >>= \xs → pure $ if (length xs == 0) then "" else "disabled"
 
 country ∷ State → HTML Event
 country state = div ! className "prompt" $ do
-  case head state.countries of
-    Nothing              -> div $ text "All done!"
-    Just (Country model) -> do
-      img ! src ("images/flags/" <> model.flag) ! alt "Flag"
-      ul ! className "countries" $ for_ (take 4 state.countries) $ answer
+  case state.countries of
+    Nothing        → button #! onClick (const Request) $ text "Start"
+    Just _         → button $ text "Test"
+    -- Just countries → liftEff $ countries >>= \countries -> case head countries of
+    --   Nothing              → pure $ div $ text "All done!"
+    --   Just (Country model) → pure $ do
+    --     img ! src ("images/flags/" <> model.flag) ! alt "Flag"
+    --     ul ! className "countries" $ for_ (take 4 countries) $ answer
 
 answer ∷ Country → HTML Event
 answer (Country model) =
--- src/js/Flag/Types.purs
+++ src/js/Flag/Types.purs
@@ -1,14 +1,16 @@
 module Flag.Types (Countries, Country(..), Name, State, Event(..)) where
 
 import Prelude (bind, ($), pure)
-import Data.Either (Either)
 import Data.Argonaut (class DecodeJson, decodeJson, (.?))
+import Control.Monad.Eff (Eff)
+import Control.Monad.Eff.Random (RANDOM)
+import Data.Maybe (Maybe)
   
 type Name = String
 type Countries = Array Country
-type State = { countries ∷ Countries, correct ∷ Int, incorrect ∷ Int }
+type State = { countries ∷ Maybe (Eff (random :: RANDOM) Countries), correct ∷ Int, incorrect ∷ Int }
 
-data Event = Respond Name | Request | Receive (Either String Countries)
+data Event = Respond Name | Request | Receive (Maybe (Eff (random :: RANDOM) Countries))
 newtype Country = Country { name :: Name, flag :: String }
 
 instance decodeJson ∷ DecodeJson Country where
