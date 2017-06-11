aac3293b990ea168247bb216fb06cd9ff07bf1b9
Wildhoney <adam.timberlake@gmail.com>
2017-06-11 18:01:47 +0100

    Unified view

--- src/js/Flag/Core.purs
+++ src/js/Flag/Core.purs
@@ -1,21 +1,22 @@
-module Flag.Core (view, foldp, init) where
+module Flag.Core (fetch, shuffle, view, foldp, init) where
 
-import Control.Monad.Aff (Aff, attempt)
+import Control.Monad.Aff (Aff)
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Random (RANDOM, random)
 import Data.Argonaut (decodeJson, Json)
-import Data.Array (length, replicate, sortBy, zip)
-import Data.Either (Either(..), either)
+import Data.Array (length, replicate, sortBy, zip, head, take, uncons)
+import Data.Either (Either(..))
+import Data.Foldable (for_)
 import Data.Maybe (Maybe(..))
 import Data.Traversable (sequence)
 import Data.Tuple (Tuple(..), snd)
-import Flag.Types (Countries, Country(..), State, Event(..))
+import Flag.Types (Countries, Country(..), Event(..), State)
 import Network.HTTP.Affjax (AJAX, get)
-import Pux (EffModel, noEffects)
+import Pux (EffModel, noEffects, onlyEffects)
 import Pux.DOM.Events (onClick)
 import Pux.DOM.HTML (HTML)
-import Text.Smolder.HTML (h1, button, ul, li, div)
-import Text.Smolder.HTML.Attributes (className)
+import Text.Smolder.HTML (h1, button, ul, li, div, img)
+import Text.Smolder.HTML.Attributes (className, src, alt)
 import Text.Smolder.Markup (text, (!), (#!))
 import Prelude hiding (div)
 
@@ -31,45 +32,35 @@ shuffle xs = do
 decode ∷ ∀ r. { response ∷ Json | r } → Either String (Array Country)
 decode request = decodeJson request.response :: Either String Countries
 
-fetch ∷ ∀ e. Aff (ajax ∷ AJAX | e) (Maybe Event)
+fetch ∷ ∀ e. Aff (random ∷ RANDOM, ajax ∷ AJAX | e) (Maybe Countries)
 fetch = do
-  response <- attempt $ get "/countries.json"
-  let countries = either (Left <<< show) decode response
-  pure $ Just $ Receive $ case shuffle <$> countries of
-    Left _   → Nothing
-    Right xs → Just xs
+  get "/countries.json" >>= \xs → case decode xs of
+    Left _          → pure $ Nothing
+    Right countries → pure $ Just countries
 
 foldp ∷ Event → State → EffModel State Event (random ∷ RANDOM, ajax ∷ AJAX)
-foldp (Respond name)      state = noEffects $ state { countries = Nothing, correct = state.correct + 1 }
+foldp (Request)           state = onlyEffects state [Just <<< Receive <$> fetch]
 foldp (Receive Nothing)   state = noEffects $ state { countries = Nothing }
 foldp (Receive (Just xs)) state = noEffects $ state { countries = Just xs }
-foldp (Request)           state = { state, effects: [fetch] }
+foldp (Respond name)      state = noEffects $ case join $ uncons <$> state.countries of
+  Nothing                              → state { countries = Nothing }
+  Just { head: (Country x), tail: xs } → case name == x.name of
+    true  → state { countries = Just xs, correct = state.correct + 1 }
+    false → state { countries = Just xs, incorrect = state.incorrect + 1 }
 
 view ∷ State → HTML Event
-view state = div ! className "flagship" $ case state.countries of
-  Nothing → toolbar state
-  Just _  → toolbar state <> country state
-
-toolbar ∷ State → HTML Event
-toolbar state = ul ! className "header" $ do
-  li $ h1 $ text "Flagship"
-  li $ button #! onClick (const Request) $ text "Start"
-  li ! className "score correct" $ text $ show state.correct
-  li ! className "score incorrect" $ text $ show state.incorrect
-  li ! className "score" $ text $ show $ state.correct + state.incorrect
-    -- where isDisabled = state.countries >>= \xs → pure $ if (length xs == 0) then "" else "disabled"
-
-country ∷ State → HTML Event
-country state = div ! className "prompt" $ do
-  case state.countries of
-    Nothing        → button #! onClick (const Request) $ text "Start"
-    Just _         → button $ text "Test"
-    -- Just countries → liftEff $ countries >>= \countries -> case head countries of
-    --   Nothing              → pure $ div $ text "All done!"
-    --   Just (Country model) → pure $ do
-    --     img ! src ("images/flags/" <> model.flag) ! alt "Flag"
-    --     ul ! className "countries" $ for_ (take 4 countries) $ answer
-
-answer ∷ Country → HTML Event
-answer (Country model) =
-  li $ button #! onClick (const $ Respond model.name) $ text model.name
+view state = div ! className "flagship" $ do
+  ul ! className "header" $ do
+    li $ h1 $ text "Flagship"
+    li $ button #! onClick (const Request) $ text "Start"
+    li ! className "score correct" $ text $ show state.correct
+    li ! className "score incorrect" $ text $ show state.incorrect
+    li ! className "score" $ text $ show $ state.correct + state.incorrect
+  <> do div ! className "prompt" $ case state.countries of
+          Nothing        → button #! onClick (const Request) $ text "Start"
+          Just countries → case head countries of
+            Nothing              → div $ text "All done!"
+            Just (Country current) → do
+              img ! src ("images/flags/" <> current.flag) ! alt "Flag"
+              ul ! className "countries" $ for_ (take 4 countries) $ \(Country answer) →
+                li $ button #! onClick (const $ Respond answer.name) $ text answer.name
