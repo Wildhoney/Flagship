b8ffbd56c3a3ed558cb5d6bd4ed1d33e730d6754
Wildhoney <adam.timberlake@gmail.com>
2017-07-03 13:28:59 +0100

    Added Flag.App

--- src/js/Main.purs
+++ src/js/Main.purs
@@ -4,11 +4,10 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Pux (CoreEffects, start)
 import Pux.Renderer.React (renderToDOM)
-import Control.Monad.Eff.Random (RANDOM)
 import Network.HTTP.Affjax (AJAX)
-import Flag.Core (view, foldp, init)
+import Flag.App (init, foldp, view)
 
-main ∷ Eff (CoreEffects (random ∷ RANDOM, ajax ∷ AJAX)) Unit
+main ∷ Eff (CoreEffects (ajax ∷ AJAX)) Unit
 main = do
   app <- start { initialState: init, view, foldp, inputs: [] }
   renderToDOM ".app" app.markup app.input
