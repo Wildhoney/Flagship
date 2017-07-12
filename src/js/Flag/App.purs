69fe00bf2536cc489c089f7a1bcb3eee6005deb2
Wildhoney <adam.timberlake@gmail.com>
2017-07-12 22:24:39 +0100

    Added initial skeleton for the app

--- src/js/Flag/App.purs
+++ src/js/Flag/App.purs
@@ -1,5 +1,7 @@
 module Flag.App (Event, Country(..), init, foldp, view) where
 
+import CSS.Transform (offset)
+import DOM.HTML.Window (RequestAnimationFrameId)
 import Data.Array (length)
 import Data.Maybe (Maybe(..))
 import Data.Newtype (class Newtype)
@@ -7,7 +9,7 @@ import Network.HTTP.Affjax (AJAX)
 import Pux (EffModel, noEffects, onlyEffects)
 import Pux.DOM.Events (onClick)
 import Pux.DOM.HTML (HTML)
-import Text.Smolder.HTML (button, div)
+import Text.Smolder.HTML (button, div, header, nav, section, a)
 import Text.Smolder.HTML.Attributes (className)
 import Text.Smolder.Markup (text, (!), (#!))
 import Prelude hiding (div)
@@ -27,14 +29,19 @@ init = { countries: [], correct: 0, incorrect: 0 }
 
 foldp :: Event -> State -> EffModel State Event (ajax :: AJAX)
 foldp (ReceiveCountries xs) state = noEffects state { countries = xs }
-foldp (RequestCountries) state = onlyEffects state [ do
+foldp (RequestCountries) state = { state: state { correct = 0, incorrect = 0 }, effects: [ do
   pure $ Just (ReceiveCountries [Country { name: "Russia", flag: "russia.svg" }]) 
-]
+] }
 
 view :: State → HTML Event
-view state =
-  div ! className "flagship" $ do
+view state = do
+  nav do
+    header $ text "Flagship"
     div ! className "total" $ text $ show (length state.countries)
     div ! className "correct" $ text $ show state.correct
     div ! className "incorrect" $ text $ show state.incorrect
-    button #! onClick (const RequestCountries) $ text "Start!"
+    a #! onClick (const RequestCountries) $ text $ case (length state.countries) of
+      0 -> "Start"
+      _ -> "Restart"
+  section do
+    div $ text "..."
