4bdaad591dd82a6e284090b55af7d881c332cca5
Wildhoney <adam.timberlake@gmail.com>
2017-07-13 07:56:35 +0100

    Added answers from foldp

--- src/js/Flag/App.purs
+++ src/js/Flag/App.purs
@@ -10,12 +10,14 @@ import Pux.DOM.HTML (HTML)
 import Text.Smolder.HTML (div, header, nav, section, a, img, ul, li)
 import Text.Smolder.HTML.Attributes (className, src)
 import Text.Smolder.Markup (text, (!), (#!))
+import Data.Tuple (Tuple(..))
 import Prelude hiding (div)
+import Data.Traversable (for_)
 
-type State = { countries :: Countries, correct :: Int, incorrect :: Int }
+type State = { all :: Countries, current :: Array String, correct :: Int, incorrect :: Int }
 type Countries = Array Country
 
-data Event = RequestCountries | ReceiveCountries Countries | GuessCountry String
+data Event = RequestCountries | ReceiveCountries (Tuple Countries (Array String)) | GuessCountry String
 
 newtype Country = Country { name :: String, flag :: String }
 derive instance newtypeCountry :: Newtype Country _
@@ -23,29 +25,28 @@ instance showCountry :: Show Country where
   show (Country { name, flag }) = "(Country { name: " <> name <> ", flag: " <> flag <> " })"
 
 init :: State
-init = { countries: [], correct: 0, incorrect: 0 }
+init = { all: [], current: [], correct: 0, incorrect: 0 }
 
 foldp :: Event -> State -> EffModel State Event (ajax :: AJAX)
 foldp (GuessCountry name) state = noEffects state { correct = state.correct + 1 }
-foldp (ReceiveCountries xs) state = noEffects state { countries = xs }
+foldp (ReceiveCountries (Tuple all current)) state = noEffects state { all = all, current = current }
 foldp (RequestCountries) state = { state: state { correct = 0, incorrect = 0 }, effects: [ do
-  pure $ Just (ReceiveCountries [Country { name: "Russia", flag: "russia.svg" }]) 
+  pure $ Just(ReceiveCountries $ Tuple
+    [Country { name: "Russia", flag: "russia.svg" }]
+    ["Ecuador", "Slovenia", "Aruba", "Indonesia"]
+  ) 
 ] }
 
 view :: State → HTML Event
 view state = do
   nav do
     header $ text "Flagship"
-    div ! className "total" $ text $ show (length state.countries)
+    div ! className "total" $ text $ show (length state.all)
     div ! className "correct" $ text $ show state.correct
     div ! className "incorrect" $ text $ show state.incorrect
-    a #! onClick (const RequestCountries) $ text $ case (length state.countries) of
+    a #! onClick (const RequestCountries) $ text $ case (length state.all) of
       0 -> "Start"
       _ -> "Restart"
   section do
     img ! src "/images/flags/indonesia.svg"
-    ul do
-      li #! onClick (const $ GuessCountry "Ecuador") $ text "Ecuador"
-      li #! onClick (const $ GuessCountry "Slovenia") $ text "Slovenia"
-      li #! onClick (const $ GuessCountry "Aruba")$ text "Aruba"
-      li #! onClick (const $ GuessCountry "Indonesia")$ text "Indonesia"
+    ul $ for_ state.current $ \name -> li #! onClick (const $ GuessCountry name) $ text name
