fcd55c6fb049ef44961e9c65a8c8f1ae764802d3
Wildhoney <adam.timberlake@gmail.com>
2017-07-17 08:43:54 +0100

    Modified startup command

--- src/server/default.js
+++ src/server/default.js
@@ -4,8 +4,7 @@ import fetch from './countries';
 
 const app = express();
 const server = http.createServer(app);
-const port = process.env.PORT || 5000;
 
 app.get('/countries.json', (_, res) => res.send(JSON.stringify(fetch())));
 app.use(express.static(`${__dirname}/..`));
-server.listen(port);
+server.listen(process.env.PORT || 5000);
